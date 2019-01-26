d72aa011699bd5b89c4ee54f79da941252191fd8
Rameş Aliyev <creator@ramesaliyev.com>
2019-01-26 18:50:06 +0300

    :sparkles: dump all globals to screen

--- draw/info.js
+++ draw/info.js
@@ -7,10 +7,43 @@ function drawInfo() {
     FPS = (1000 / (now - prevDrawTime)).toFixed(0);
   }
 
-  const options = {align: 'right', color: COLOR.infoTextColor};
-  const right = screenWidth - 20;
+  const table = [
+    ['FPS', FPS],
+    ['screenWidth', screenWidth],
+    ['screenHeight', screenHeight],
+    ['viewportWidth', viewportWidth],
+    ['viewportHeight', viewportHeight],
+    ['viewportOffsetX', viewportOffsetX],
+    ['viewportOffsetY', viewportOffsetY],
+    ['centerWorldX', centerWorldX],
+    ['centerWorldY', centerWorldY],
+    ['centerScreenX', centerScreenX],
+    ['centerScreenY', centerScreenY],
+    ['mouseWorldX', mouseWorldX],
+    ['mouseWorldY', mouseWorldY],
+    ['mouseScreenX', mouseScreenX],
+    ['mouseScreenY', mouseScreenY],
+    ['mouseDownOriginScreenX', mouseDownOriginScreenX],
+    ['mouseDownOriginScreenY', mouseDownOriginScreenY],
+    ['mouseDownOriginWorldX', mouseDownOriginWorldX],
+    ['mouseDownOriginWorldY', mouseDownOriginWorldY],
+    ['isMouseDown', isMouseDown],
+    ['isMouseOutOfScreen', isMouseOutOfScreen],
+  ];
 
-  rect(screenWidth - 230, 0, 230, 175, {fill: true, color: COLOR.infoBoxColor});
+  const right = screenWidth - 55;
+  rect(screenWidth - 235, 0, 235, (table.length + 1) * 25, {fill: true, color: COLOR.infoBoxColor});
+  rect(screenWidth - 235, 0, 235, (table.length + 1) * 25, {fill: false, color: COLOR.infoTextColor});
 
-  text(right, 25, `FPS:${FPS}`, options);
+  table.forEach((row, index) => {
+    let [name, val] = row;
+
+    if (typeof val === 'number') {
+      val = Math.floor(val);
+    }
+
+    text(right, (index+1) * 25, `${name}`, {align:'right', color: COLOR.infoTextColor});
+    text(right + 10, (index+1) * 25, `${val}`, {align:'left', color: COLOR.infoTextColor});
+  })
+
 }
